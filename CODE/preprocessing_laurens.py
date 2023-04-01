66d5ea68901fab4de3d023c691368b7d624e85a9
aronadam <aronadam@live.nl>
2023-04-01 21:22:50 +0200

    Create separate error plots

--- CODE/preprocessing_laurens.py
+++ CODE/preprocessing_laurens.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import normalize
 import cvxpy as cp
+from timeit import default_timer as t_rec
+import csv
 
 
 def fat_matrix(M, N=784):
@@ -57,38 +59,83 @@ def progression_plot(x):
     plt.show()
 
 
-def NMSE_plot(X):
+def NMSE_plot(X_in,y,n_samples=100):
 
     # Number of samples (rows in the fat matrix)
     M_list = [50, 100, 200, 300, 400, 500, 600, 700, 784]
 
-    X_index = range(len(X))
+    labels = np.linspace(0,9,10)
 
+    
     NMSE_lists = [[] for _ in range(len(M_list))]
+    mean_NMSE = np.zeros([9,len(labels)])
+    times = np.zeros([9,len(labels)])
+    for label in labels:
+            
+        X = X_in[y==label]
+        X = X[:n_samples]
+        X_index = range(len(X))
+        # Compute NMSE for each M for various X's
+        for i in range(len(M_list)):
+            t_loop_1 = t_rec()
+            for k in X_index:
+                x = X[k]
+                reconstructed = one_bit_cs_by_lp(M_list[i], x)
+                MSE = np.mean((x - reconstructed) ** 2)
+                NMSE = MSE / (np.linalg.norm(x) ** 2)
+                NMSE_lists[i].append(NMSE)
+                # print(f"Reconstructed X_{k} with {M_list[i]} samples")
+            t_loop_2=t_rec()
+            times[i,int(label)] = (t_loop_2-t_loop_1)/(k+1)
+            print(f"For label {int(label)}, all reconstructions finished for A =     {M_list[i]}     x 784.\nThis operation took {round(t_loop_2-t_loop_1,2)} seconds.\n")
+        # Average the NMSE for each M
+        mean_NMSE[:,int(label)] = [np.mean(NMSE_list) for NMSE_list in NMSE_lists]
 
-    # Compute NMSE for each M for various X's
-    for i in range(len(M_list)):
-        for k in X_index:
-            x = X[k]
-            reconstructed = one_bit_cs_by_lp(M_list[i], x)
-            MSE = np.mean((x - reconstructed) ** 2)
-            NMSE = MSE / (np.linalg.norm(x) ** 2)
-            NMSE_lists[i].append(NMSE)
-            print(f"Reconstructed X_{k} with {M_list[i]} samples")
-
-    # Average the NMSE for each M
-    mean_NMSE = [np.mean(NMSE_list) for NMSE_list in NMSE_lists]
-
-    # Plot the mean NMSE for each M
-    plt.plot(M_list, mean_NMSE, marker=">")
+        # Plot the mean NMSE for each M
+        plt.plot(M_list, mean_NMSE[:,int(label)], marker=">")
+        plt.xlabel('Number of samples')
+        plt.ylabel('NMSE (dB)')
+        plt.title(f'Error propagation for pictures of digit {int(label)}')
+        plt.ylim(0.0)
+        plt.show(block=False)
+        plt.savefig(f'C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\CODE\\Images\\NMSE_{int(label)}.png')
+        plt.pause(2)
+        plt.clf()
+        plt.close()
+    mean_NMSE_overall = [np.mean(mean_NMSE) for mean_NMSE in mean_NMSE]
+    with open('C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\CODE\\tmp_file.txt', 'w') as f:
+        csv.writer(f, delimiter=',').writerows(times)
+            
+    plt.plot(M_list, mean_NMSE_overall, marker=">")
+    plt.xlabel('Number of samples')
+    plt.ylabel('NMSE (dB)')
+    plt.title(f'Overall error propagation')
     plt.ylim(0.0)
     plt.show()
 
+def reconstruction_plots(X,y):
+    reconstructed = np.zeros([10,784,5])
+    original = np.zeros([784,5])
+    for label in y:
+        label = int(label)
+        x = (X[y==label])[0]
+        original[:,i] = x.copy()
+        M_list = [50, 100,  500, 700, 784]
+        for i in range(len(M_list)):
+            reconstructed[label,:,i] = one_bit_cs_by_lp(M_list[i], x)
+    
+
+
+
 
 if __name__ == "__main__":
-
-    mnist = pd.read_csv("../DATA/mnist_train.csv", header=None).to_numpy()
-
+    
+    mnist = pd.read_csv("C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\DATA\\mnist_test.csv", header=None).to_numpy()
     X, y = mnist[:, 1:], mnist[:, 0]
+     
+    t_start = t_rec()
+    NMSE_plot(X,y)
+    t_end = t_rec()
+    print(f"Full Operation took {round((t_end-t_start),2)} seconds")
 
-    NMSE_plot(X[0:5])
+    reconstruction_plots(X,y)
