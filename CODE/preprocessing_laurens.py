f857f845380e17907e951b132fdd9ea82fca334e
aronadam <aronadam@live.nl>
2023-04-06 13:05:46 +0200

    create time plots and work on MAP estimate

--- CODE/preprocessing_laurens.py
+++ CODE/preprocessing_laurens.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import normalize
 import cvxpy as cp
 from timeit import default_timer as t_rec
 import csv
+from sklearn.metrics import average_precision_score as MAP_score
 
 
 def fat_matrix(M, N=784):
@@ -59,7 +60,7 @@ def progression_plot(x):
     plt.show()
 
 
-def NMSE_plot(X_in,y,n_samples=100):
+def NMSE_plot(X_in,y,n_samples=50):
 
     # Number of samples (rows in the fat matrix)
     M_list = [50, 100, 200, 300, 400, 500, 600, 700, 784]
@@ -90,6 +91,8 @@ def NMSE_plot(X_in,y,n_samples=100):
             print(f"For label {int(label)}, all reconstructions finished for A =     {M_list[i]}     x 784.\nThis operation took {round(t_loop_2-t_loop_1,2)} seconds.\n")
         # Average the NMSE for each M
         mean_NMSE[:,int(label)] = [np.mean(NMSE_list) for NMSE_list in NMSE_lists]
+        with open(f'C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\CODE\\times_{int(label)}.txt', 'w') as f:
+            csv.writer(f, delimiter=',').writerows(times)
 
         # Plot the mean NMSE for each M
         plt.plot(M_list, mean_NMSE[:,int(label)], marker=">")
@@ -103,8 +106,6 @@ def NMSE_plot(X_in,y,n_samples=100):
         plt.clf()
         plt.close()
     mean_NMSE_overall = [np.mean(mean_NMSE) for mean_NMSE in mean_NMSE]
-    with open('C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\CODE\\tmp_file.txt', 'w') as f:
-        csv.writer(f, delimiter=',').writerows(times)
             
     plt.plot(M_list, mean_NMSE_overall, marker=">")
     plt.xlabel('Number of samples')
@@ -112,6 +113,94 @@ def NMSE_plot(X_in,y,n_samples=100):
     plt.title(f'Overall error propagation')
     plt.ylim(0.0)
     plt.show()
+
+def MAP_plot(X_in,y,n_samples=2):
+
+    # Number of samples (rows in the fat matrix)
+    M_list = [50, 100, 200, 300, 400, 500, 600, 700, 784]
+
+    labels = np.linspace(0,9,10)
+
+    
+    MAP_list = [[] for _ in range(len(M_list))]
+    mean_MAP = np.zeros([9,len(labels)])
+    times = np.zeros([9,len(labels)])
+    for label in labels:
+            
+        X = X_in[y==label]
+        X = X[:n_samples]
+        X_index = range(len(X))
+        # Compute NMSE for each M for various X's
+        for i in range(len(M_list)):
+            t_loop_1 = t_rec()
+            for k in X_index:
+                x = X[k]
+                reconstructed = one_bit_cs_by_lp(M_list[i], x)
+                print(type(reconstructed))
+                print(reconstructed)
+                reconstructed[reconstructed<128]=0
+                reconstructed[reconstructed>=128]=1
+                x[x<128] =0
+                x[x>=128] =1
+                # print(reconstructed)
+                MAP = MAP_score(x,reconstructed)
+                # NMSE = MSE / (np.linalg.norm(x) ** 2)
+                # convert input to 1 bit input before 1B map comparison
+                MAP_list[i].append(MAP)
+                # print(f"Reconstructed X_{k} with {M_list[i]} samples")
+            t_loop_2=t_rec()
+            # times[i,int(label)] = (t_loop_2-t_loop_1)/(k+1)
+            print(f"For label {int(label)}, all reconstructions finished for A =     {M_list[i]}     x 784.\nThis operation took {round(t_loop_2-t_loop_1,2)} seconds.\n")
+        # Average the NMSE for each M
+        mean_MAP[:,int(label)] = [np.mean(MAP_list) for MAP_list in MAP_list]
+        # with open(f'C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\CODE\\times_{int(label)}.txt', 'w') as f:
+        #     csv.writer(f, delimiter=',').writerows(times)
+
+        # Plot the mean NMSE for each M
+        plt.plot(M_list, mean_MAP[:,int(label)], marker=">")
+        plt.xlabel('Number of samples')
+        plt.ylabel('MAP score')
+        plt.title(f'MAP score propagation for pictures of digit {int(label)}')
+        plt.ylim(0.0)
+        plt.show(block=False)
+        plt.savefig(f'C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\CODE\\Images\\MAP_{int(label)}.png')
+        plt.pause(3)
+        plt.clf()
+        plt.close()
+    MAP_overall = [np.mean(mean_MAP) for mean_MAP in mean_MAP]
+            
+    plt.plot(M_list, MAP_overall, marker=">")
+    plt.xlabel('Number of samples')
+    plt.ylabel('NMSE (dB)')
+    plt.title(f'Overall MAP propagation')
+    plt.ylim(0.0)
+    plt.show()
+
+def compute_avg_sparsity(X_in,y,n_samples=1):
+
+    # Number of samples (rows in the fat matrix)
+    labels = np.linspace(0,9,10)
+    avg_s = np.zeros([10,1])
+    for label in labels:
+        sparsity = np.zeros([n_samples,1])
+        X = X_in[y==label]
+        X = X[:n_samples]
+        X_index = range(len(X))
+        # Compute NMSE for each M for various X's
+        
+        for k in X_index:
+            x = X[k]
+            sparsity[k] = np.count_nonzero(x==0)
+
+        avg_s[int(label)] = np.sum(sparsity)/len(X)
+        print(f"Average sparsity level for number {int(label)}: {(int(avg_s[int(label)]))} out of 784\nLeaves {(int(avg_s[int(label)])-784)} nonzeros")
+    overall_average = int(np.average(avg_s))
+    print(f"Overall average sparsity level: {overall_average} Out of 784.\nLeaves on average {(784-overall_average)} nonzero entries")
+
+
+
+
+
 
 def reconstruction_plots(X,y):
     reconstructed = np.zeros([10,784,5])
@@ -123,19 +212,50 @@ def reconstruction_plots(X,y):
         M_list = [50, 100,  500, 700, 784]
         for i in range(len(M_list)):
             reconstructed[label,:,i] = one_bit_cs_by_lp(M_list[i], x)
-    
 
+def make_sim_times_plot(path):
+    data = np.genfromtxt(path, delimiter=',')
+    M_list = np.array([50, 100, 200, 300, 400, 500, 600, 700, 784])
+    sizes = data.shape[0]
+    labels = data.shape[1]
+    legends=[]
+    for i in range(labels):
+        maximum = np.max(data[:,i])
+        plt.plot(M_list,data[:,i]/maximum,linewidth=".5")
+        plt.title(F"Normalized Simulation times over number of measurements")
+        legends.append(f"{i}, norm = {round(maximum,2)}")
+    plt.legend(legends)
+    plt.xlabel("Number of measurements")
+    plt.ylabel(f"Normalized simulation time per measurement [s]")
+    plt.show(block=False)
+    plt.savefig(f'C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\CODE\\Images\\sim_times_1.png')
+    plt.pause(3)
+    plt.clf()
+    plt.close()
 
-
+    mean_times = np.sum(data,axis=1)
+    plt.plot(M_list,mean_times,color="red")
+    plt.title(F"Mean simulation times over different number of measurements for all labels")
+    plt.xlabel("Number of measurements")
+    plt.ylabel("Simulation time per measurement [s]")
+    plt.show(block=False)
+    plt.savefig(f'C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\CODE\\Images\\sim_times_2.png')
+    plt.pause(3)
+    plt.clf()
+    plt.close()
 
 if __name__ == "__main__":
-    
-    mnist = pd.read_csv("C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\DATA\\mnist_test.csv", header=None).to_numpy()
+
+    mnist = pd.read_csv("C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\DATA\\mnist_train.csv", header=None).to_numpy()
     X, y = mnist[:, 1:], mnist[:, 0]
-     
+
     t_start = t_rec()
-    NMSE_plot(X,y)
+    MAP_plot(X,y)
+    path = "C:\\Users\\arona\\OneDrive\\Bureaublad\\boeken\\1_DC\\DataCompression_project\\CODE\\times_9.txt"
+    # make_sim_times_plot(path)
+    # compute_avg_sparsity(X,y,50)
+
     t_end = t_rec()
     print(f"Full Operation took {round((t_end-t_start),2)} seconds")
 
-    reconstruction_plots(X,y)
+    # reconstruction_plots(X,y)
